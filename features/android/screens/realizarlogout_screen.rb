054bd87f377453100dba52a70a4f5d277c8fc12f
CS-Luccas-Xavier <luccas.xavier@concrete.com.br>
2017-07-20 13:37:03 -0300

    Arrumando logout

--- features/android/screens/realizarlogout_screen.rb
+++ features/android/screens/realizarlogout_screen.rb
@@ -1,19 +1,17 @@
 class RealizarlogoutScreen < AndroidScreenBase
   # The screen identificator
-  trait(:trait)                 { "* id:'#{btn_logout}'" }
+  trait(:trait)                 { "* id:'#{tv_actual_value}'" }
 
   # Declare all the elements of this screen
+  element(:tv_actual_value)         { 'text_view_price_last_purchase' }
   element(:btn_logout)         { 'title' }
-  #element(:btn_logout)         { 'item_logout' }
-  #element(:btn_menu)           { 'menu_home' }
 
   # Declare all actions of this screen
   def touch_btn_menu
-    touch_screen_element(btn_menu)
+    tap_when_element_exists("android.support.v7.widget.ActionMenuView index:0")
   end
 
   def touch_btn_logout
     touch_screen_element(btn_logout)
   end
-
 end
